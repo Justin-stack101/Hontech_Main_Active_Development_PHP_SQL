a171d60f0e9f235a6f3394a8d8fe961d617fcf6e
Justin Nolasco <154451690+Justin-stack101@users.noreply.github.com>
2026-08-23 10:56:52 +0800

    chore(security): untrack local scratch debug scripts from git

--- scratch/search.js
+++
@@ -1,5 +0,0 @@
-import fs from 'fs';
-const lines = fs.readFileSync('frontend/index.html', 'utf8').split('\n');
-lines.slice(1600, 1750).forEach((line, idx) => {
-    console.log(`${idx + 1601}: ${line}`);
-});
